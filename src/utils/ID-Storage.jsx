a3ecf4fe724e5015ba368211a5bc613d577cb7ec
Akhmatova <3088421890@qq.com>
2025-05-15 23:56:14 +0800

    update:utils

--- src/utils/ID-Storage.jsx
+++ src/utils/ID-Storage.jsx
@@ -1,14 +1,12 @@
-// 存储用户 ID
+
 export function saveUserId(userId) {
     sessionStorage.setItem('userId', userId);
 }
 
-// 获取存储的用户 ID
 export function getUserId() {
     return sessionStorage.getItem('userId');
 }
 
-// 清除用户 ID
 export function clearUserId() {
     sessionStorage.removeItem('userId');
 }
